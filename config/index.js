6fd72a6cfc8108b0715af9840b402cfe4bac9fdf
CaitlinFergie7 <91681430+CaitlinFergie7@users.noreply.github.com>
2021-09-30 16:21:47 +0100

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,6 @@
+export const config = {
+    API_URL: "localhost:3000",
+    COLLECTION_NAME: "NEDS",
+    COLLECTION_TITLE: "Non-Educated Delinquents",
+    COLLECTION_DESCRIPTION: '10k "Young Team Hooligans"-neds.',
+};
